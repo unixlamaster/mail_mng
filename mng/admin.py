d09ddaeaa88b95d843e5348f173d33e7405a0c2e
unixlamaster <unixlamaster@mail.ru>
2016-10-22 18:33:29 +0300

    modified:   mng/admin.py 	modified:   mng/models.py

--- mng/admin.py
+++ mng/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Virtual_domain
+from .models import Virtual_user
 
-# Register your models here.
+admin.site.register(Virtual_domain)
+admin.site.register(Virtual_user)
--- mng/models.py
+++ mng/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Virtual_domain(models.Model):
 	name = models.CharField(max_length=50)
@@ -6,7 +7,7 @@ class Virtual_domain(models.Model):
 		db_table = "virtual_domains"
 
 class Virtual_user(models.Model):
-	domain_id = models.ForeignKey(Virtual_domain, on_delete=models.CASCADE)
+	domain = models.ForeignKey(Virtual_domain, on_delete=models.CASCADE)
 	user = models.CharField(max_length=40)
 	password = models.CharField(max_length=32)
 	firstname = models.CharField(max_length=100)
@@ -18,6 +19,6 @@ class Virtual_user(models.Model):
 	born = models.CharField(max_length=10)
 	tel_w2 = models.CharField(max_length=45)
 	notice = models.CharField(max_length=15)
-	pass_change = models.DateTimeField
+	pass_change = models.DateTimeField(default=timezone.now)
 	class Meta:
 		db_table = "virtual_users"
