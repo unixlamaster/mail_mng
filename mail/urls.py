c652b85f2f1dd8120a485d978872e46850b25893
unixlamaster <unixlamaster@mail.ru>
2016-12-19 16:49:12 +0300

    new file:   mail/settings.py 	modified:   mail/urls.py 	modified:   mng/admin.py 	modified:   mng/models.py 	modified:   mng/views.py

--- mail/urls.py
+++ mail/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^mng/', include('mng.urls')),
 	url(r'^$', views.index, name='index'),
 ]
