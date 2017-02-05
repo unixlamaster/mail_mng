0e7ad4807c887669932ff001c202541230d9fa55
unixlamaster <unixlamaster@mail.ru>
2017-02-05 22:57:21 +0300

    commit 2

---
+++ mng/admin_tags.py
@@ -0,0 +1,20 @@
+@register.filter 
+def pdb(element):
+    import pdb; pdb.set_trace()
+    return element
+    
+def virtualuser_list(cl):
+    """
+    Displays the headers and data list together
+    """
+    headers = list(result_headers(cl))
+    num_sorted_fields = 0
+    for h in headers:
+        if h['sortable'] and h['sorted']:
+            num_sorted_fields += 1
+    return {'cl': cl,
+            'result_hidden_fields': list(result_hidden_fields(cl)),
+            'result_headers': headers,
+            'num_sorted_fields': num_sorted_fields,
+            'results': list(results(cl))}
+
